dfc422222cd2e24d07f9c480608b4f0ab6e653c7
krutoi-perets <vorobushek.mudrinov@gmail.com>
2025-05-09 19:25:46 +0300

    готова

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.findMinButton = new System.Windows.Forms.Button();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.showTimeButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(29, 12);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.ReadOnly = true;
+            this.textBoxMin.Size = new System.Drawing.Size(96, 20);
+            this.textBoxMin.TabIndex = 0;
+            // 
+            // findMinButton
+            // 
+            this.findMinButton.Location = new System.Drawing.Point(142, 10);
+            this.findMinButton.Name = "findMinButton";
+            this.findMinButton.Size = new System.Drawing.Size(100, 23);
+            this.findMinButton.TabIndex = 1;
+            this.findMinButton.Text = "Найти минимум";
+            this.findMinButton.UseVisualStyleBackColor = true;
+            this.findMinButton.Click += new System.EventHandler(this.findMinButton_Click);
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Location = new System.Drawing.Point(29, 51);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.ReadOnly = true;
+            this.textBoxTime.Size = new System.Drawing.Size(96, 20);
+            this.textBoxTime.TabIndex = 2;
+            // 
+            // showTimeButton
+            // 
+            this.showTimeButton.Location = new System.Drawing.Point(142, 51);
+            this.showTimeButton.Name = "showTimeButton";
+            this.showTimeButton.Size = new System.Drawing.Size(100, 23);
+            this.showTimeButton.TabIndex = 3;
+            this.showTimeButton.Text = "Показать время";
+            this.showTimeButton.UseVisualStyleBackColor = true;
+            this.showTimeButton.Click += new System.EventHandler(this.showTimeButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(29, 136);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(209, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(90, 107);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 5;
+            this.loadButton.Text = "Загрузить";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 167);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.showTimeButton);
+            this.Controls.Add(this.textBoxTime);
+            this.Controls.Add(this.findMinButton);
+            this.Controls.Add(this.textBoxMin);
+            this.Name = "Form1";
+            this.Text = "ЛР 6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.Button findMinButton;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Button showTimeButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button loadButton;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("(название)\n" +
+                "(ваши фамилии) 23ВП1\nВариант 8", "Лабораторная работа 6");
+        }
+
+        // Поиск минимума
+        private async void findMinButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            List<int> numbers = new List<int>();
+            int arraySize = 1000;
+            for (int i = 0; i < arraySize; i++)
+                numbers.Add(rnd.Next(-1000, 10000));
+
+            int min = await FindMinAsync(numbers);
+
+            textBoxMin.Text = $"Минимум: {min}";
+        }
+        private async Task<int> FindMinAsync(List<int> numbers)
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1500);
+                return numbers.Min();
+            });
+        }
+
+        // Вывод системного времени
+        private CancellationTokenSource timeTokenSource;
+        private async void showTimeButton_Click(object sender, EventArgs e)
+        {
+            if (timeTokenSource != null)
+            {
+                timeTokenSource.Cancel();
+                timeTokenSource = null;
+                showTimeButton.Text = "Показать время";
+                return;
+            }
+
+            timeTokenSource = new CancellationTokenSource();
+            showTimeButton.Text = "Остановить время";
+
+            await ShowTimeAsync(timeTokenSource.Token);
+        }
+        private async Task ShowTimeAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                textBoxTime.Invoke((MethodInvoker)(() =>
+                {
+                    textBoxTime.Text = DateTime.Now.ToLongTimeString();
+                }));
+
+                await Task.Delay(1000);
+            }
+        }
+
+        // Имитация загрузки
+        private async void loadButton_Click(object sender, EventArgs e)
+        {
+            await SimulateLoadingAsync();
+        }
+        private async Task SimulateLoadingAsync()
+        {
+            progressBar1.Value = 0;
+            for (int i = 0; i <= 100; i += 10)
+            {
+                progressBar1.Value = i;
+                await Task.Delay(300);
+            }
+
+            MessageBox.Show("Загрузка завершена!", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            progressBar1.Value = 0;
+        }
+    }
+}
