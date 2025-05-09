045b34c9dbe47f19caa4dffb2419a8af8a974356
krutoi-perets <vorobushek.mudrinov@gmail.com>
2025-05-09 09:11:36 +0300

    готова

--- Lab4/StackListener.cs
+++ Lab4/StackListener.cs
@@ -16,16 +16,15 @@ namespace Lab2
 
             stack.StackAdded += (TransportCompany company) =>
             {
-                dataGridView.Rows.Clear();
-                dataGridView.RowCount = 1;
-                dataGridView.Rows[0].Cells[0].Value = company.name;
-                dataGridView.Rows[0].Cells[1].Value = company.pricePerKilometer;
-                dataGridView.Rows[0].Cells[2].Value = company.averageDeliveryTime;
-                dataGridView.Rows[0].Cells[3].Value = company.yearFounded;
-                dataGridView.Rows[0].Cells[4].Value = company.transportedMass;
-                dataGridView.Rows[0].Cells[5].Value = company.rating;
-                dataGridView.Rows[0].Cells[6].Value = company.phoneNumber;
-                dataGridView.Rows[0].Cells[7].Value = company.GetDeliveryInfo();
+                int rowIndex = dataGridView.Rows.Add();
+                dataGridView.Rows[rowIndex].Cells[0].Value = company.name;
+                dataGridView.Rows[rowIndex].Cells[1].Value = company.pricePerKilometer;
+                dataGridView.Rows[rowIndex].Cells[2].Value = company.averageDeliveryTime;
+                dataGridView.Rows[rowIndex].Cells[3].Value = company.yearFounded;
+                dataGridView.Rows[rowIndex].Cells[4].Value = company.transportedMass;
+                dataGridView.Rows[rowIndex].Cells[5].Value = company.rating;
+                dataGridView.Rows[rowIndex].Cells[6].Value = company.phoneNumber;
+                dataGridView.Rows[rowIndex].Cells[7].Value = company.GetDeliveryInfo();
                 objCount.Text = TransportCompany.countObj.ToString();
             };
             stack.StackRemoved += () =>
